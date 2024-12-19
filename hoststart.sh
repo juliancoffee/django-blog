8a430d7a67cc229da5ed90b31c9af6be9f12efea
juliancoffee <lightdarkdaughter@gmail.com>
2024-12-19 21:59:47 +0200

    Change the command in hoststart.sh to `exec`

--- hoststart.sh
+++ hoststart.sh
@@ -5,4 +5,4 @@ export DEVMODE=1
 
 # clear debug logfile before staring
 echo "" > debug.log
-python -m gunicorn -w 4 mysite.wsgi --bind 0.0.0.0:8000 --access-logfile -
+exec gunicorn -w 4 mysite.wsgi --bind 0.0.0.0:8000 --access-logfile -
