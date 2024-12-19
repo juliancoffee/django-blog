d1cd53005985bc62dd97685a00df3a0d04f9012f
juliancoffee <lightdarkdaughter@gmail.com>
2024-12-19 22:43:21 +0200

    (render) Explicitly enable accesslog

--- hoststart.sh
+++ hoststart.sh
@@ -5,8 +5,11 @@ export DEVMODE=1
 
 # clear debug logfile before staring
 echo "" > debug.log
+
+# ok, render.com seems to enable --access-logfile by default, but I'd rather
+# make it explcit
 exec gunicorn mysite.wsgi \
     -w 4 \
     --bind 0.0.0.0:8000 \
-    --log-level debug
-    #--access-logfile - \
+    --log-level debug \
+    --access-logfile -
