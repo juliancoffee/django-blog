2136f250a38d89adc0a0961252e379bb35da0c3b
juliancoffee <lightdarkdaughter@gmail.com>
2025-01-11 22:40:46 +0200

    Add logout button
    - And use reverse_lazy as next_page for LoginView.
      Technically that means that we don't need any regex there, but I
      decided to left it there anyway, if just as an example.

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -1,6 +1,13 @@
 import logging
 
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseRedirect,
+)
+from django.urls import reverse, reverse_lazy
 
 logger = logging.getLogger(__name__)
 
@@ -8,4 +15,11 @@ logger = logging.getLogger(__name__)
 class LoginView(DjangoLoginView):
     template_name = "blog/login.html"
     # if can't figure out the next page, redirect to home
-    next_page="/index"
+    #
+    # NOTE: can't use reverse() here, because it will lead to circular imports
+    next_page = reverse_lazy("blog:index")
+
+
+def instant_logout(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return HttpResponseRedirect(reverse("blog:index"))
--- blog/urls.py
+++ blog/urls.py
@@ -7,12 +7,14 @@ import blog.post.views
 app_name = "blog"
 urlpatterns = [
     # post views
-    re_path("^$|index/", blog.post.views.index, name="index"),
+    # NOTE: if using regex, don't forget ^ to match from the begining
+    re_path("^$|^index/", blog.post.views.index, name="index"),
     path("<int:post_id>/", blog.post.views.detail, name="detail"),
     path("<int:post_id>/comment/", blog.post.views.comment, name="comment"),
     # auth
     # TODO: that could be a separate Django app?
     path("accounts/login/", blog.auth.views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", blog.auth.views.instant_logout, name="logout"),
     # misc
     path("debug_view/", blog.debug.views.debug_view, name="debug_view"),
 ]
