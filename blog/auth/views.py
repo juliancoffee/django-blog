de37f120cfd6d1bc4258a265f2759a8c92a6b048
juliancoffee <lightdarkdaughter@gmail.com>
2025-01-11 22:12:47 +0200

    Use Django's default LoginView
    Use Django's LoginView class with a few tweaks, of course.
    - Change template name to "blog/login.html"
    - Change default next_page to "/index", cause we don't have account page and
      frankly I think it is silly, even if we had.
      People log in to interact with the website, not to check their account's
      profile page.
      Unless it's a social network, but even then you'd want to go to your
      profile, not your account settings.

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -1,23 +1,11 @@
 import logging
 
-from django.contrib.auth.decorators import login_not_required
-from django.http import (
-    HttpRequest,
-    HttpResponse,
-)
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as DjangoLoginView
 
 logger = logging.getLogger(__name__)
 
 
-@login_not_required
-def login(request: HttpRequest) -> HttpResponse:
-    next_page = request.GET.get("next")
-    logging.debug(f"{next_page=}")
-    return render(
-        request,
-        "blog/login.html",
-        {
-            "next": next_page,
-        },
-    )
+class LoginView(DjangoLoginView):
+    template_name = "blog/login.html"
+    # if can't figure out the next page, redirect to home
+    next_page="/index"
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 import blog.auth.views
 import blog.debug.views
@@ -7,12 +7,12 @@ import blog.post.views
 app_name = "blog"
 urlpatterns = [
     # post views
-    path("", blog.post.views.index, name="index"),
+    re_path("^$|index/", blog.post.views.index, name="index"),
     path("<int:post_id>/", blog.post.views.detail, name="detail"),
     path("<int:post_id>/comment/", blog.post.views.comment, name="comment"),
     # auth
     # TODO: that could be a separate Django app?
-    path("accounts/login/", blog.auth.views.login),
+    path("accounts/login/", blog.auth.views.LoginView.as_view(), name="login"),
     # misc
-    path("debug_view", blog.debug.views.debug_view, name="debug_view"),
+    path("debug_view/", blog.debug.views.debug_view, name="debug_view"),
 ]
